06e3a4ed709d498aeefa38ad34a140f634ab8428
Yukti Kathuria <yukti.kathuria@gmail.com>
2021-02-15 22:43:14 -0500

    Updated items.py

--- powell/items.py
+++ powell/items.py
@@ -9,16 +9,15 @@ import scrapy
 class PowellItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    item = PowellItem()
-    item['title'] = title
-    item['price'] = price
-    item['author'] = author
-    item['category'] = category
-    item['quantity'] = quantity
-    item['stores'] = stores
-    item['pub_date'] = pub_date
-    item['pub'] = pub
-    item['rating'] = rating
-    item['comment'] - comment
+    title = scrapy.Field()
+    price = scrapy.Field()
+    author = scrapy.Field()
+    category = scrapy.Field()
+    quantity = scrapy.Field()
+    stores = scrapy.Field()
+    pub_date = scrapy.Field()
+    pub = scrapy.Field()
+    rating = scrapy.Field()
+    comment = scrapy.Field()
 
     yield item
